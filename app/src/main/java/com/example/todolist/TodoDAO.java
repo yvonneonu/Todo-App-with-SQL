df94cd6731c534f5126b6443fc25aa356fec5172
yvonneonu <yvonneonuorah@gmail.com>
2020-12-29 22:21:53 +0100

    NEW

--- app/src/main/java/com/example/todolist/TodoDAO.java
+++ app/src/main/java/com/example/todolist/TodoDAO.java
@@ -1,7 +1,18 @@
 package com.example.todolist;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface TodoDAO {
+    @Query("SELECT * FROM task")
+    LiveData<List<TodoModel>>getAllTasks();
+
+    @Insert
+    void Insert (TodoModel todoModel);
+
 }
